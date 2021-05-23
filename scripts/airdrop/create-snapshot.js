cdfee1aa73a1db3a37aade366792670d839d70e9
MrClottom <philippe@dumo.net>
2021-05-23 10:14:52 +0200

    Fix snapshot generation script

--- scripts/airdrop/create-snapshot.js
+++ scripts/airdrop/create-snapshot.js
@@ -3,14 +3,12 @@ const web3 = require('./web3')()
 const { connectDB, Transfer, ASCENDING } = require('./mongoose')(web3)
 const { constants, saveJson, loadJson, uniFarmFactories, ether } = require('./utils')(web3)
 const { ZERO_ADDRESS, SCALE } = constants
-const otherFarms = require('./other-farms.json')
 
+const BLACKLIST_SRC = './blacklist-info.json'
 const ZERO = new BN('0')
-const emtpyBnObj = () => {
-  const newObj = {}
-  newObj.get = (address) => newObj[address] ?? new BN('0')
-  return newObj
-}
+const getBN = (obj, key) => obj[key] ?? ZERO
+
+const blacklistInfo = loadJson(BLACKLIST_SRC)
 
 async function getLogIndexParams(pair, toBlock) {
   const match = { $match: { pair, blockNumber: { $lte: toBlock } } }
@@ -31,21 +29,31 @@ async function getLogIndexParams(pair, toBlock) {
   }
 }
 
-async function getPairRewards(pair, toBlock) {
+async function getBlacklistedAddresses(pair) {
+  const cachedBlacklist = blacklistInfo.cachedBlacklists[pair]
+  if (cachedBlacklist) {
+    return new Set(cachedBlacklist)
+  }
   const uniPoolsRes = await Promise.all(
     uniFarmFactories.map(async (farmFactory) => {
       const { pool } = await farmFactory.methods.pools(pair).call()
       return pool
     })
   )
-  const blacklistedAddresses = new Set(
-    [...uniPoolsRes, ...otherFarms].filter((address) => address !== ZERO_ADDRESS)
+  const newBlacklistedAddresses = new Set(
+    [...uniPoolsRes, ...blacklistInfo.otherFarms].filter((address) => address !== ZERO_ADDRESS)
   )
-  console.log('blacklistedAddresses: ', blacklistedAddresses)
+  blacklistInfo.cachedBlacklists[pair] = [...newBlacklistedAddresses]
+  saveJson(BLACKLIST_SRC, blacklistInfo, [null, 2])
+  return newBlacklistedAddresses
+}
+
+async function getPairRewards(pair, toBlock) {
+  const blacklistedAddresses = await getBlacklistedAddresses(pair)
 
   const { logIndexDelta, blockNumShift } = await getLogIndexParams(pair, toBlock)
   const transfers = await Transfer.aggregate([
-    { $match: { pair } },
+    { $match: { pair, blockNumber: { $lte: toBlock } } },
     {
       $project: {
         absoluteIndex: {
@@ -67,12 +75,13 @@ async function getPairRewards(pair, toBlock) {
   ])
 
   const newRewards = {}
-  const balances = emtpyBnObj()
-  const userDebt = emtpyBnObj()
+  const balances = {}
+  const userDebt = {}
   let totalSupply = ZERO
   let totalAccumulator = ZERO
 
-  let lastBlock = transfers[0].blockNumber
+  let lastBlock = transfers[transfers.length - 1].blockNumber
+  console.log('lastBlock: ', lastBlock)
   // const finalBlock = 9
   const finalBlock = toBlock
 
@@ -90,12 +99,10 @@ async function getPairRewards(pair, toBlock) {
   }
 
   const accountRewards = (user) => {
-    const userBalance = balances.get(user)
+    const userBalance = getBN(balances, user)
     if (userBalance.gt(ZERO)) {
-      setRewards(
-        user,
-        (newRewards[user] ?? ZERO).add(totalAccumulator.mul(userBalance)).sub(userDebt.get(user))
-      )
+      const debt = getBN(userDebt, user)
+      setRewards(user, getBN(newRewards, user).add(totalAccumulator.mul(userBalance)).sub(debt))
     }
   }
 
@@ -106,11 +113,11 @@ async function getPairRewards(pair, toBlock) {
   }
 
   const increaseBalance = (user, amount) => {
-    setBalance(user, balances.get(user).add(amount))
+    setBalance(user, getBN(balances, user).add(amount))
   }
 
   const decreaseBalance = (user, amount) => {
-    setBalance(user, balances.get(user).sub(amount))
+    setBalance(user, getBN(balances, user).sub(amount))
   }
 
   const updateAccumulator = (currentBlockNumber) => {
@@ -123,6 +130,7 @@ async function getPairRewards(pair, toBlock) {
   }
 
   for (let { from, to, value, blockNumber } of transfers) {
+    if (blockNumber > toBlock) break
     value = new BN(value)
 
     updateAccumulator(blockNumber)
@@ -150,12 +158,11 @@ async function getPairRewards(pair, toBlock) {
   }
 
   let totalRewards = ZERO
-  for (const user of Object.keys(newRewards)) {
-    const userBalance = balances.get(user)
+  for (const [user, userBalance] of Object.entries(balances)) {
     if (userBalance.gt(ZERO)) {
       decreaseBalance(user, userBalance)
     }
-    totalRewards = totalRewards.add(newRewards[user])
+    totalRewards = totalRewards.add(newRewards[user] ?? ZERO)
   }
 
   const finalNewRewards = []
@@ -184,7 +191,7 @@ async function main() {
   let currentPair = 0
   let totalRewards = ZERO
   for (const [pair, weight] of Object.entries(pairs)) {
-    console.log(`${++currentPair}/${totalPairs} ${pair}`)
+    console.log(`\n${++currentPair}/${totalPairs} ${pair}`)
     const newRewards = await getPairRewards(pair, toBlock)
     for (const [user, userPairRewardsShare] of Object.entries(newRewards)) {
       const additionalRewards = userPairRewardsShare.mul(weight)
